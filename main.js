eea87f3a8ac23468991a75b30f01574ac6838e82
Tojiyevvd <tojiyevd33@gmail.com>
2023-05-12 20:07:34 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,94 @@
+
+1
+// var a = prompt("a ni kriting");
+// var p = 0;
+// p = 4*a;
+// alert(p);
+
+2
+// var a =prompt ("a ni kriting");
+// var S = 0;
+// S = 4*a;
+// alert(S);
+
+3
+// var a = prompt ("a ni kriting")*1;
+// var b = prompt (" b ni kriting")*1;
+// var s = a*b;
+// var p = 2*(a+b);
+// alert(s);
+// alert(p);
+
+4
+// var d =prompt("d ni kriting");
+// var L = 0;
+// var p = 3.14;
+// L = p*d;
+// alert(L);
+
+5
+// var a = prompt("a ni kriting");
+// var V = 0;
+// var S = 0;
+//  V = a*a*a;
+//  S = 6*a*a;
+//  alert(S);
+//  alert(V);
+
+6
+// var a = prompt("a ni kriting")*1;
+// var b = prompt("b ni kriting")*1;
+// var c = prompt("c ni kriting")*1;
+ 
+// var V = 0;
+// var S = 0;
+
+// V = a*b*c;
+// S = 2*(a*b+b*c+a*c);
+// alert(V);
+// alert(S);
+
+7
+// var R = prompt("R ni kriting");
+// var L = 0;
+// var S = 0;
+// var p = 3.14;
+// L = 2*p*R;
+// S = p*R*R;
+// alert(L);
+// alert(S);
+
+8
+// var a = prompt("a ni kriting")*1;
+// var b = prompt("b ni kriting")*1;
+
+// S = 0;
+// S = (a+b)/2
+// alert(S);
+
+9
+// var a = prompt("a ni kriting")*1;
+// var b = prompt("b ni kriting")*1;
+
+//  var S = 0;
+// S = Math.sqrt(a+b);
+// alert(S);
+
+10
+// var a = prompt("a ni kriting")*1;
+// var b = prompt("b ni kriting")*1;
+
+// var d = 0;
+// var c = 0;
+// var e = 0;
+// var f = 0;
+
+// d = a*b;
+// c = a+b;
+// e = Math.pow(a,2)
+// f = Math.pow(b,2)
+// alert(d);
+// alert(c);
+// alert(e);
+// alert(f);
+
